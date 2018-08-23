d180c95b9d0cfc5ce43666c320234c8d283163c7
artemholovnia <artemholovnia@artemholovnia.localdomain>
2018-08-23 14:27:41 +0200

    bridges and users

--- ari_controller/tasks.py
+++ ari_controller/tasks.py
@@ -9,7 +9,7 @@ import asyncio, json
 def get_active_channels():
     group_name='channels_group'
     channel_layer = get_channel_layer()
-    active_channels = requests.get('http://192.168.7.17:8088/ari/channels', auth=HTTPBasicAuth('roip_ari', 'roip_ari')).text
+    active_channels = requests.get('http://192.168.7.72:8088/ari/channels', auth=HTTPBasicAuth('roip_ari', 'roip_ari')).text
     channels_dict = {'group_name':group_name, 'celery_response':True, 'data':active_channels}
     AsyncToSync(channel_layer.group_send)(group_name, {'type':'send.celery', 'message':channels_dict})
 
